2a091acbdc50eeac8fa03137236dfb08bb5739fe
joshuasaetern <155663061+joshuasaetern@users.noreply.github.com>
2024-01-04 08:51:02 -0800

    Initial commit

---
+++ stuff/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿// Joshua Saetern
+// 01/02/2024
+// Lab 1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Namespace is the project name 
+namespace HelloWorld
+{// Start of the namespace
+    internal class Program
+    {// Start of the class
+        static void Main(string[] args)
+        {// Start of the main method - code that runs automatically
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }// End of the class
+}// End of the namespace
